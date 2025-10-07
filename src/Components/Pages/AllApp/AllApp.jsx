8b14a179d0dd79a05a69801ce5fce92fabf92c1a
Omar Faruk <khfarukcht@mail.com>
2025-10-08 01:12:21 +0600

    allApp card add

--- src/Components/Pages/AllApp/AllApp.jsx
+++ src/Components/Pages/AllApp/AllApp.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import DownloadImg from '../../../assets/icon-downloads.png'
+import StarImg from '../../../assets//icon-ratings.png'
 
 const AllApp = ({data}) => {
+    const {image , title, ratingAvg , downloads} = data;
     return (
-        <div>
-            <h1>{data.title}</h1>
-        </div>
+           <div className="card bg-base-100 shadow-xl p-5">
+                <img
+                className="rounded-lg"
+                  src={image}
+                  alt="apps"/>
+           
+              <div>
+                <h2 className="text-2xl font-semibold py-5">{title}</h2>
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-2 text-[#00D390] bg-[#F1F5E8] py-2 px-4 rounded-lg">
+                    <img className="w-[15px]" src={DownloadImg} alt="downloadImage" />
+                    <p>{downloads}</p>
+                  </div>
+                  <div className="flex items-center gap-2 text-[#FF8811] bg-[#FFF0E1] py-2 px-4 rounded-lg ">
+                    <img className="w-[15px]" src={StarImg} alt="downloadImage" />
+                    <p>{ratingAvg}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
     );
 };
 
--- src/Components/Pages/Apps/Apps.jsx
+++ src/Components/Pages/Apps/Apps.jsx
@@ -14,14 +14,14 @@ const Apps = () => {
             <h1 className='text-5xl font-semibold'>Our All Applications</h1>
             <p className='text-gray-600 py-5'>Explore All Apps on the Market developed by us. We code for Millions</p>
             </div>
-            <div className='flex items-center justify-between mb-5 text-xl font-semibold'>
+            <div className='flex items-center md:flex-row flex-col gap-5 justify-between mb-5 text-xl font-semibold'>
                 <p><span>(0)</span> Apps Found</p>
                 <label className=" outline-none border-1 tabs-xl flex px-4 py-2 border-gray-200 rounded-lg items-center gap-4">
                     <CiSearch className='text-xl' />
                  <input type="search"  className='text-lg outline-none' required placeholder='Search' /> 
                 </label>
             </div>
-        <div className='grid xl:grid-cols-4'>
+        <div className='grid xl:grid-cols-4 gap-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mb-20'>
             {
             allAppsData.map(data => <AllApp key={data.id} data ={data}></AllApp>)
         }
